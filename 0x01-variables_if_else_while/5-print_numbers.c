3ec69721dcc82621c879e8ee4dd71aa57029c1ee
isri21 <israelalemu21@gmail.com>
2022-12-10 01:43:55 +0300

    added 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ * Return: zero
+ */
+
+int main(void)
+{
+	int l;
+
+	for (l = 0; l < 10; l++)
+		printf("%d", l);
+	printf("\n");
+
+	return (0);
+}
