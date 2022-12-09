03c9b115f7e7bc503cc2a73d989277935885866b
isri21 <israelalemu21@gmail.com>
2022-12-10 02:08:13 +0300

    added 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ * Return: zero
+ */
+
+
+int main(void)
+{
+
+
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	
+		if (i == '9')
+			break;
+	
+		putchar(',');
+	
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
