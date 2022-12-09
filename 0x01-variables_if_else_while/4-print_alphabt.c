5d6a3fd04efb9dbe11b3d1a373eb384a2899577c
isri21 <israelalemu21@gmail.com>
2022-12-10 01:41:21 +0300

    added 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet except q and e
+ * Return: zero
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'e' || c == 'q')
+			continue;
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
