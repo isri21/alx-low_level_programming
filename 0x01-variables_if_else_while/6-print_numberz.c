6ced656fce9843db31865091534e1ea10e6ba235
isri21 <israelalemu21@gmail.com>
2022-12-10 01:55:33 +0300

    added 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - This is the main function
+* Return: Returns 0
+*/
+
+int main(void)
+{
+
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+
+	return (0);
+
+}
