528e3a2f35f4010b7d15b8c663b57738d124940f
isri21 <israelalemu21@gmail.com>
2022-12-10 02:31:26 +0300

    added 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ * Return: Returns 0
+ */
+
+int main(void)
+{
+	int i;
+	int l;
+	int s;
+	int t;
+
+	i = 0;
+	l = i;
+	while (i < 10)
+	{
+		while (l < 10)
+		{
+			s = i;
+			t = l + 1;
+			while (s < 10)
+			{
+				while (t < 10)
+				{
+					putchar(i + '0');
+					putchar(l + '0');
+					putchar(' ');
+					putchar(s + '0');
+					putchar(t + '0');
+					if ((i == 9) && (l == 8) && (s == 9) && (t == 9))
+						;
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					t++;
+				}
+				t = 0;
+				s++;
+			}
+			l++;
+		}
+		l = 0;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
