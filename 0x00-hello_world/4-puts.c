54dd2744878932d0432e0877312a60864ee3de70
isri21 <israelalemu21@gmail.com>
2023-10-05 16:38:29 +0300

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program.
+*
+* Description: prints exactly "Programming is like building a
+* multilingual puzzle, followed by a new line.
+* Return: 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
