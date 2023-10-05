a844541a3289e06a190f186a3633b45c40bfb117
isri21 <israelalemu21@gmail.com>
2023-10-05 16:57:15 +0300

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program.
+*
+* Description: prints exactly "with proper grammar,
+* but the outcome is a piece of art,", followed by a new line.
+* Return: 0
+*/
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
