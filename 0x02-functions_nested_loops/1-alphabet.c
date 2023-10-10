6f0c550a44e5c96304194507abfb70e5da1c47d6
isri21 <israelalemu21@gmail.com>
2023-10-10 21:55:18 +0300

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet - Prints the alphabet, in lowercase, followed by a new line.
+*
+* Return: 0
+*/
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
