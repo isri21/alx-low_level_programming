124c7919cf7abac82ebcac61cabbf87551476e14
isri21 <israelalemu21@gmail.com>
2023-10-17 01:05:44 +0300

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts - prints a string followed by a new line.
+* @str: pointer to the string to be printed.
+*
+* Return: void.
+*/
+void _puts(char *str)
+{
+	puts(str);
+}
