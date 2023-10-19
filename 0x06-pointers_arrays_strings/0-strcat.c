e04cd94a9711727fa93a064ebfa5b026b3afa3e0
isri21 <israelalemu21@gmail.com>
2023-10-19 22:18:26 +0300

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strcat - conactenates two strings
+* @dest: one of the strings to concatenate
+* @src: other string to concatenate
+*
+* Return: Concatenated string
+*/
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
