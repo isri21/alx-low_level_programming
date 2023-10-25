9f248acfed8c10597a62056fd4a1ad4472d580b0
isri21 <israelalemu21@gmail.com>
2023-10-25 23:52:24 +0300

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strlen_recursion - gets the length of a string
+* @s: string to be evaluated
+*
+* Return: length of the string
+*/
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
 #ifndef main_h
 #define main_h
 void _puts_recursion(char *s);
-
+int _strlen_recursion(char *s);
 
 
 
