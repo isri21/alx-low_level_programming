a5a2261c4f8bea37f405fa0214d2a7f584908b68
isri21 <israelalemu21@gmail.com>
2023-11-07 03:16:14 +0300

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - defines a dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
