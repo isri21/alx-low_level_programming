08e0f26d7a02b3105c66aaef3972d8ad68f7b6e3
isri21 <israelalemu21@gmail.com>
2023-11-25 02:00:41 +0300

    removed task 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdlib.h>
+#include <unistd.h>
 unsigned int binary_to_uint(const char *b);
 int _strlen(const char *s);
-
+int _putchar(char c);
+void print_binary(unsigned long int n);
 #endif
